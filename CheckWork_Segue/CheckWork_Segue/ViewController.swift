58db27b6c4009e02c0bc0eb826791fd05734bd12
gil0304 <ryogo5.0304@gmail.com>
2023-11-14 16:45:36 +0900

    add: checkwork&answer

---
+++ CheckWork_Segue/CheckWork_Segue/ViewController.swift
@@ -0,0 +1,49 @@
+//MARK: ①セグエで画面遷移ができるようにしよう
+//MARK: ②prepareForSegueで値の受け渡しができるようにしよう
+//MARK: ③dismissで前の画面に戻ることができるようにしよう
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var timeLabel: UILabel!
+    var timer: Timer!
+    var count: Double = 10
+    var score: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(up), userInfo: nil, repeats: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func tapButton() {
+        
+        score = score + 1
+    }
+    
+    @objc func up() {
+        
+        count = count - 0.05
+        timeLabel.text = String(format: "%.2f", count)
+        
+        if count <= 0 {
+            timer.invalidate()
+            
+            // Segueを使って画面遷移する
+            
+        }
+    }
+    
+    //画面遷移するときに呼ばれるメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 画面遷移先はResultViewController
+        // 画面遷移時の値受け渡しの処理はここに書こう
+        
+    }
+
+}
+
